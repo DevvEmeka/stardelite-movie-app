e16867e56ed6c38b9f56e2c56504dbbe617b4c35
DevvEmeka <devemeka@outlook.com>
2025-01-07 03:36:22 +0100

    install react icon and API setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import { useEffect } from "react";
 
 // a4c0a4fb
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=a4c0a4fb'
+// Defining the movie API
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a4c0a4fb";
 
 function App() {
+  const searchMovie = async (result) => {
+    const response = await fetch(`${API_URL}&s=${result}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovie("Batman");
+  }, []);
   return <>App</>;
 }
 
